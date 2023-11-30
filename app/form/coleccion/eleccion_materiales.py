63e98944f81d4f40bacb9cb55d0548d13baa6abf
Martin Ezequiel Rodriguez <martinrodrig2001@gmail.com>
2023-11-30 09:14:31 -0300

    guardo cambio para agregar fotos

---
+++ app/form/coleccion/eleccion_materiales.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import DateField, SubmitField, ValidationError
+from wtforms.validators import DataRequired
+from datetime import date
+
+
+class FormEleccionMateriales(FlaskForm):
+    fecha_entrega = DateField(
+        "Fecha de entrega de materiales",
+        default=date.today,
+        validators=[DataRequired(message="El campo para la fecha de recepción es obligatorio")],
+    )
+    enviar = SubmitField("Guardar")
+
+    def validate_fecha_entrega(self, field):
+        if field.data <= date.today():
+            raise ValidationError(
+                "La fecha de recepción de materiales debe ser posterior a la fecha actual"
+            )
